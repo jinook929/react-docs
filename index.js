ce4c65538faa8419e2d6b36a9bc9e3c600455042
jinook929 <jinook929@gmail.com>
2019-05-18 19:29:54 -0400

    7. Conditional Rendering

--- index.js
+++ index.js
@@ -297,8 +297,8 @@ function ActionLink() {
     // "<strong>Lasers Activated!!!</strong>";
   }
 
-  function handleClick(e) {
-    e.preventDefault();
+  function handleClick(event) {
+    event.preventDefault();
   }
 
   return (
@@ -309,7 +309,7 @@ function ActionLink() {
         Activate Lasers
       </button>
       <div id="handleClickMsg">Lasers ready</div>
-      {/* how to prevent default behavior in React */}
+      {/* how to prevent default behavior in React (Create a function 'handleClick' with 'event.preventDefault()') */}
       <a href="#" onClick={handleClick}>
         Click me
       </a>
@@ -349,3 +349,182 @@ class Toggle extends React.Component {
 }
 
 ReactDOM.render(<Toggle />, document.getElementById('root06_'));
+
+
+
+// 7. Conditional Rendering
+function UserGreeting (props) {
+  return (
+    <div>
+      <h2>Welcome Back!</h2>
+    </div>
+  );
+}
+
+function GuestGreeting (props) {
+  return (
+    <div>      
+      <h2>Please Sign Up.</h2>
+    </div>
+  );
+}
+
+function Greeting(props) {
+  const isLoggedIn = props.isLoggedIn;
+  if(isLoggedIn) {
+    return <UserGreeting />;
+  } else {
+    return <GuestGreeting />;
+  }
+}
+
+ReactDOM.render((
+  <div>
+    <h1>07. Conditional Rendering</h1>
+    <Greeting isLoggedIn={true}/>
+    <p>(Fixed Response)</p>
+  </div>), 
+  document.getElementById('root07'))
+
+function LoginButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Login
+    </button>
+  )
+}
+
+function LogoutButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Logout
+    </button>
+  )
+}
+
+class LoginControl extends React.Component {
+  constructor(props) {
+    super(props);
+    // this.handleLoginClick = this.handleLoginClick.bind(this);
+    // this.handleLogoutClick = this.handleLogoutClick.bind(this);
+    this.state = {isLoggedIn: false}
+  }
+
+  handleLoginClick = () => {
+    this.setState({isLoggedIn: true});
+  }
+
+  handleLogoutClick = () => {
+    this.setState({isLoggedIn: false});
+  }
+
+  render() {
+    const isLoggedIn = this.state.isLoggedIn;
+    let button;
+
+    if(isLoggedIn) {
+      button = <LogoutButton onClick={this.handleLogoutClick} />
+    } else {
+      button = <LoginButton onClick={this.handleLoginClick} />
+    }
+    return (
+      <div>
+        <Greeting isLoggedIn={isLoggedIn} />
+        {button}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<LoginControl />, document.getElementById('root07_'))
+
+function Mailbox(props) {
+  const unreadMessages = props.unreadMessages;
+
+  return (
+    <div>
+      <h2>Hello!</h2>
+      {unreadMessages.length > 0 && 
+        <div>
+          <h3>You have {unreadMessages.length} unread messages.</h3>
+      <p>The title of your most recent message is "{unreadMessages[(unreadMessages.length - 1)]}"</p>
+        </div>
+      }
+    </div>
+  );
+}
+
+const messages = ['React', 'Re: React', 'Re: Re: React', 'Hello', 'Good Morning Friend'];
+
+ReactDOM.render(<Mailbox unreadMessages={messages} />, document.getElementById('root07__'))
+
+class LoginStatus extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {isLoggedIn: false}
+  }
+
+  handleLoginBtnClick = () => {
+    this.setState({isLoggedIn: !this.state.isLoggedIn})
+  }
+
+  render() {
+    let isLoggedIn = this.state.isLoggedIn
+    console.log('this.state.isLoggedIn @ LoginStatus render():', this.state.isLoggedIn)
+    return (
+      <div>
+        {isLoggedIn ? (
+          <p>The user is <strong>currently</strong> logged in.</p>
+        ) : (
+          <p>The user is <strong>not</strong> logged in.</p>
+        )}
+        <button onClick={this.handleLoginBtnClick}>Login</button>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<div><LoginStatus /></div>, document.getElementById('root07___'))
+
+function WarningBanner(props) {
+  console.log("WarningBanner function")
+  if(!props.warn) {
+    return null;
+  }
+
+  return (
+    <div className="warning">
+      warning!
+    </div>
+  );
+}
+
+class Page extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {showWarning: false}
+    console.log('constructor')
+  }
+
+  handleToggleClick = () => {
+    console.log('handleToggleClick')
+    this.setState(state => ({
+      showWarning: !state.showWarning
+    }));
+  }
+
+  render() {
+    console.log('render')
+    console.log(this.state.showWarning)
+    return (
+      <div>
+        <WarningBanner warn={this.state.showWarning} />
+        <button onClick={this.handleToggleClick}>
+          {this.state.showWarning ? 'Hide' : 'Show'}
+        </button>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<Page />, document.getElementById('root07____'))
